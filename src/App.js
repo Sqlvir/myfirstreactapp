b72775bf7a5a9573a2c5fd98e76a05daef381ced
Virendranath Yadav <virendranathyadav@Virendranaths-MacBook-Pro.local>
2024-02-16 23:55:30 +0530

    Add new page component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Myaccount from './Components/Myaccount';
+import Mytestpage from './Components/Mytestpage';
 function App() {
   return (
     <div className="App">
       <Myaccount />
+      <Mytestpage />
     </div>
   );
 }
---
+++ src/Components/Mytestpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mytestpage = () => {
+  return (
+    <div>
+      <h1>My first test page</h1>
+    </div>
+  )
+}
+
+export default Mytestpage
